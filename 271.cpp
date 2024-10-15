589bb69ec3a227a130dc4294662cae30c6d20b2a
marcogroot <mg.marcogroot@gmail.com>
2024-10-15 21:05:19 +1030

    271

---
+++ 271.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+
+// https://leetcode.com/problems/contains-duplicate/
+// 15-10-2024 @ marcogroot
+
+using namespace std;
+
+string encode(vector<string>& strs) {
+    string result = "";
+    for (const string& str : strs) {
+        result += to_string(str.length()) + '#' + str;
+    }
+    return result;
+}
+
+vector<string> decode(string s) {
+    vector<string> output;
+    int i = 0;
+    while (i < s.size()) {
+        int j = i;
+        while (s[j] != '#') {
+            j++;
+        }
+        int length = stoi(s.substr(i, j - i));
+        string str = s.substr(j + 1, length);
+        output.push_back(str);
+        i = j + 1 + length;
+    }
+    return output;
+}
+
+int main() {
+    cout << " asdfa" << endl;
+    vector<string> strs = {"we","say",":","yes","!@#$%^&*()"};
+    string encoded = encode(strs);
+    vector<string> decoded = decode(encoded);
+    cout << encoded << endl;
+    for (string& s : decoded) {
+        cout <<  s << " ";
+    }
+    cout << "\n";
+}
