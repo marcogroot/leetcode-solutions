4b8b66bdab89cf19be9315f35c2c78acb6c1f7bc
marcogroot <mg.marcogroot@gmail.com>
2024-10-15 20:38:14 +1030

    add gitignore

--- 0_template.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <unordered_map>
-
-// https://leetcode.com/problems/contains-duplicate/
-// 15-10-2024 @ marcogroot
-
-using namespace std;
