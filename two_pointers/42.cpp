d327a15ee34a0af8ac8d0fdf9a38e3e596d46d98
marcogroot <mg.marcogroot@gmail.com>
2024-10-18 17:17:20 +1030

    trapping rain water and container with most  water

---
+++ two_pointers/42.cpp
@@ -0,0 +1,41 @@
+#include <ios>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+
+// https://leetcode.com/problems/trapping-rain-water/description/
+// 18-10-2024 @ marcogroot
+
+using namespace std;
+
+int trap(vector<int>& height) {
+    if (height.empty()) {
+        return 0;
+    }
+
+    int left { 0 };
+    int right = { static_cast<int>(height.size() - 1) };
+    int max_left { height[left] };
+    int max_right { height[r] };
+    int answer = 0;
+
+    while (left < right) {
+        if (max_left < max_right) {
+            left++;
+            max_left = max(max_left, height[left]);
+            answer += max_left - height[left];
+        } else {
+            right--;
+            max_right = max(max_right, height[right]);
+            answer += max_right - height[right];
+        }
+    }
+    return answer;
+}
+
+
+int main() {
+    vector<int> input = {4, 2, 3};
+    trap(input);
+}
