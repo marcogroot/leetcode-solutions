5f7bb21e5ccc62b34a55ec4758b30ebf08786086
Marco Groot <mg.marcogroot@gmail.com>
2025-02-28 11:22:39 +1030

    daily

--- misc/873.cpp
+++ misc/873.cpp
@@ -10,22 +10,33 @@
 
 using namespace std;
 
-int lenLongestFibSubseq(vector<int> &A) {
-  int n = A.size();
-  map<int, int> m;
-  for (int i = 0; i < n; i++)
-    m[A[i]] = i;
-  vector<vector<int>> dp(n, vector<int>(n, 2));
-  for (int j = 1; j < n; j++)
-    for (int i = 0; i < j; i++)
-      if (m.find(A[j] - A[i]) != m.end() and m[A[j] - A[i]] < i)
-        dp[i][j] = max(dp[i][j], dp[m[A[j] - A[i]]][i] + 1);
-  int ans = 0;
-  for (int i = 0; i < n; i++)
-    for (int j = i + 1; j < n; j++)
-      ans = max(ans, dp[i][j]);
+int lenLongestFibSubseq(vector<int> &nums) {
+  unordered_set<int> s;
+  for (int x : nums) {
+    s.insert(x);
+  }
 
-  return (ans == 2) ? 0 : ans;
+  int answer = 0;
+
+  for (int i = 0; i < nums.size(); i++) {
+    for (int j = i + 1; j < nums.size(); j++) {
+      int a = nums[i];
+      int b = nums[j];
+
+      int c = a + b;
+
+      int count = 2;
+      while (s.contains(c)) {
+        a = b;
+        b = c;
+        c = a + b;
+        count++;
+        answer = max(answer, count);
+      }
+    }
+  }
+
+  return answer;
 }
 
 int main() { vector<int> nums = {1, 2, 3, 4, 5}; }
